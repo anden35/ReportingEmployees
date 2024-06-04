3c88c05e81cd1602204535aa57c6cf153a67ba7f
Adam Landen <landen.adam@gmail.com>
2024-06-04 06:44:52 -0500

    First commit with semi-working changes.  Report working with employees except for those with null reports.

---
+++ src/main/java/com/mindex/challenge/data/Compensation.java
@@ -0,0 +1,37 @@
+package com.mindex.challenge.data;
+
+import java.time.LocalDate;
+
+public class Compensation {
+    private Employee employee;
+    private Double salary;
+    private LocalDate effectiveDate;
+
+    public Compensation(){
+    }
+
+    public Employee getEmployee() {
+        return employee;
+    }
+
+    public void setEmployee(Employee employee) {
+        this.employee = employee;
+    }
+
+    public Double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Double salary) {
+        this.salary = salary;
+    }
+
+    public LocalDate getEffectiveDate() {
+        return effectiveDate;
+    }
+
+    public void setEffectiveDate(LocalDate effectiveDate) {
+        this.effectiveDate = effectiveDate;
+    }
+
+}
---
+++ src/main/java/com/mindex/challenge/service/impl/ReportingStructureServiceImpl.java
@@ -0,0 +1,39 @@
+package com.mindex.challenge.service.impl;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.mindex.challenge.dao.EmployeeRepository;
+import com.mindex.challenge.data.Employee;
+import com.mindex.challenge.data.ReportingStructure;
+import com.mindex.challenge.service.ReportingStructureService;
+
+@Service
+public class ReportingStructureServiceImpl implements ReportingStructureService {
+
+    private static final Logger LOG = LoggerFactory.getLogger(ReportingStructureServiceImpl.class);
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @Override
+    public ReportingStructure read(String id) {
+        LOG.debug("Querying total number of reporting employees under the employee with id [{}]", id);
+
+        Employee employee = employeeRepository.findByEmployeeId(id);
+
+        if (employee == null) {
+            throw new RuntimeException("Invalid employeeId: " + id);
+        }
+
+        ReportingStructure report = new ReportingStructure();
+        report.setEmployee(employee);
+        // Find the direct reports (tree nodes of employee's direct reports)
+        // Need to handle null direct reports
+        report.setNumberOfReports(employee.getDirectReports().size());
+        return report;
+    }
+
+}
